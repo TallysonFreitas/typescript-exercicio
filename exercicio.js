d52cbf3a03c4a9be8e2f024deef30597f7b09c5b
Tallyson Freitas <tallyson.eu@outlook.com>
2023-07-20 19:16:51 -0300

    exercicio ebac

---
+++ exercicio.js
@@ -0,0 +1,7 @@
+"use strict";
+const multiplicacao = (valor1, valor2) => {
+    return valor1 * valor2;
+};
+function concatenar(nome) {
+    return 'ola, ' + nome;
+}
---
+++ exercicio.ts
@@ -0,0 +1,7 @@
+const multiplicacao = (valor1:number,valor2:number):number => {
+    return valor1 * valor2
+}
+
+function concatenar(nome:string):string{
+    return 'ola, ' + nome
+}
